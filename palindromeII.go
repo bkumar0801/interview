cded159ae1da05547fe66ac438f30d38f4ad8751
Bijendra Kumar <kumarbijendra@gmail.com>
2020-09-25 00:13:50 +0200

    Create palindromeII.go

---
+++ palindromeII.go
@@ -0,0 +1,46 @@
+/*
+ Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+
+Input: "aba"
+Output: True
+
+Example 2:
+
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+*/
+
+func isPalindrome(s string) bool {
+    if len(s) == 0 {
+        return false
+    }
+    
+    middleIndex := len(s) / 2
+    rev := len(s[:middleIndex]) -1
+    for _, v := range s[middleIndex+1:] {
+        if rune(s[rev]) != v {
+            return false
+        }
+        rev = rev - 1
+    }
+    return true
+}
+
+func validPalindrome(s string) bool {
+    if len(s) == 0 {
+        return true
+    }
+    
+    if len(s) % 2 == 0 {
+        for i, _ := range s {
+            if isPalindrome(s[i+1:]) {
+                return true
+            }
+        }
+        return false
+    }
+    return isPalindrome(s)
+}
